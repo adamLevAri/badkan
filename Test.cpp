a036ae245f0a696bbbab1461630aabeff56a1dd5
adamLevAri <37263805+adamLevAri@users.noreply.github.com>
2020-03-07 22:34:29 +0200

    Update Test.cpp

--- Test.cpp
+++ Test.cpp
@@ -26,6 +26,20 @@ TEST_CASE("Test case 1") {
 		CHECK(T.find("adam", "hava") == string("husband"));
 		CHECK(T.find("adam", "posher") == string("grandfather"));
 		
+    
+}
+
+TEST_CASE("Test case 2") {
+    FamilyTree T;
+
+	T.addNew("adam", 'm');
+	T.addNew("hava", 'f', 'w', "adam");
+	T.addNew("shem", 'm', 'c', "adam");
+	T.addNew("ham", 'm', 'c', "hava");
+	T.addNew("yefet", 'm', 's', "ham");
+	T.addNew("kar", 'f', 'w', "ham");
+	T.addNew("posher", 'm', 'c', "ham");
+	
 		CHECK(T.findRelation("posher", 'g') == string("adam"));
 		CHECK(T.findRelation("posher", 'u') == string("yefet"));
 		CHECK(T.find("ham", "posher") == string("father"));
@@ -37,9 +51,5 @@ TEST_CASE("Test case 1") {
 		CHECK(T.find("ham", "posher") == string("father"));
 		CHECK(T.find("adam", "hava") == string("husband"));
 		CHECK(T.find("adam", "posher") == string("grandfather"));
-    
-}
-
-TEST_CASE("Test case 2") {
-    /* Add more tests here */
+	
 }
