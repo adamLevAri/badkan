1209212c926da4efecb1e9d753121c24dc379e34
adamLevAri <37263805+adamLevAri@users.noreply.github.com>
2020-03-07 22:37:20 +0200

    Update Test.cpp

--- Test.cpp
+++ Test.cpp
@@ -30,26 +30,26 @@ TEST_CASE("Test case 1") {
 }
 
 TEST_CASE("Test case 2") {
-    FamilyTree T;
+    FamilyTree T2;
 
-	T.addNew("adam", 'm');
-	T.addNew("hava", 'f', 'w', "adam");
-	T.addNew("shem", 'm', 'c', "adam");
-	T.addNew("ham", 'm', 'c', "hava");
-	T.addNew("yefet", 'm', 's', "ham");
-	T.addNew("kar", 'f', 'w', "ham");
-	T.addNew("posher", 'm', 'c', "ham");
+	T2.addNew("adam", 'm');
+	T2.addNew("hava", 'f', 'w', "adam");
+	T2.addNew("shem", 'm', 'c', "adam");
+	T2.addNew("ham", 'm', 'c', "hava");
+	T2.addNew("yefet", 'm', 's', "ham");
+	T2.addNew("kar", 'f', 'w', "ham");
+	T2.addNew("posher", 'm', 'c', "ham");
 	
-		CHECK(T.findRelation("posher", 'g') == string("adam"));
-		CHECK(T.findRelation("posher", 'u') == string("yefet"));
-		CHECK(T.find("ham", "posher") == string("father"));
-		CHECK(T.find("adam", "hava") == string("husband"));
-		CHECK(T.find("adam", "posher") == string("grandfather"));
+		CHECK(T2.findRelation("posher", 'g') == string("adam"));
+		CHECK(T2.findRelation("posher", 'u') == string("yefet"));
+		CHECK(T2.find("ham", "posher") == string("father"));
+		CHECK(T2.find("adam", "hava") == string("husband"));
+		CHECK(T2.find("adam", "posher") == string("grandfather"));
 		
-		CHECK(T.findRelation("posher", 'g') == string("adam"));
-		CHECK(T.findRelation("posher", 'u') == string("yefet"));
-		CHECK(T.find("ham", "posher") == string("father"));
-		CHECK(T.find("adam", "hava") == string("husband"));
-		CHECK(T.find("adam", "posher") == string("grandfather"));
+		CHECK(T2.findRelation("posher", 'g') == string("adam"));
+		CHECK(T2.findRelation("posher", 'u') == string("yefet"));
+		CHECK(T2.find("ham", "posher") == string("father"));
+		CHECK(T2.find("adam", "hava") == string("husband"));
+		CHECK(T2.find("adam", "posher") == string("grandfather"));
 	
 }
